ad7aa38a91cbd22ce3e7bcf3034b0e5a9bb7411a
Md. Nakibul Islam Shaki <nakibulshaki@gmail.com>
2020-09-17 19:18:30 +0600

    Upload to souce control

---
+++ Singleton-Design-Pattern/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton_Design_Pattern
+{
+   public sealed class Calculator
+    {
+        int i = 1;
+        private Calculator()
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+        private static Calculator instance = null;
+        public static  Calculator Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Calculator();
+                }
+                return instance;
+            }
+        }
+        public double ValueOne { get; set; }
+        public double ValueTwo { get; set; }
+        public double Addition()
+        {
+            return ValueOne + ValueTwo;
+        }
+    }
+}
---
+++ Singleton-Design-Pattern/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Singleton_Design_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Instance.ValueOne = 10.5;
+            Calculator.Instance.ValueTwo = 5.5;
+            Console.WriteLine("Addition : " + Calculator.Instance.Addition());
+            
+            Console.WriteLine("\n----------------------\n");
+            Calculator.Instance.ValueOne = 20.0;
+            Calculator.Instance.ValueTwo = 10.5;
+            Console.WriteLine("Addition : " + Calculator.Instance.Addition());
+            Console.WriteLine("\n----------------------\n");
+            Calculator.Instance.ValueOne = 10.0;
+            Calculator.Instance.ValueTwo = 15.5;
+            Console.WriteLine("Addition : " + Calculator.Instance.Addition());
+
+            Console.ReadLine();
+        }
+    }
+}
